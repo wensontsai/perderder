d9e8544509c00668bebe85bf4a3c0fa3e4803188
Wenson Tsai <wensontsai@gmail.com>
2015-10-26 17:51:54 -0400

    its on

--- app/scripts/components/dropdown/src/dropdown.jsx
+++ app/scripts/components/dropdown/src/dropdown.jsx
@@ -9,6 +9,11 @@ var Dropdown = React.createClass({
 	handleClick: function(){
 		alert('dropdown yoo');
 	},
+	getInitialState: function(){
+		return {
+			open: false
+		}
+	},
 	render: function(){
 		var list = this.props.items.map(function(item){
 			return <ListItem item={item} />
@@ -21,7 +26,7 @@ var Dropdown = React.createClass({
 					title={this.props.title} 
 					subTitleClassName="caret" 
 					/>
-				<ul>
+				<ul className={"dropdown-menu " + (this.state.open ? "show" : '')} > 
 					{list}
 				</ul>
 			</div>
